adcf7085c68086c65d7b0ba424014c46031581c1
Ángel <anrealba9@gmail.com>
2021-04-22 20:02:04 -0600

    Listo solo para graficar

--- proyectoCovid/Backend/API/PacientesNC.php
+++ proyectoCovid/Backend/API/PacientesNC.php
@@ -41,16 +41,19 @@ session_start();
         case 'GET':
             // $paciente = Paciente::getPaciente();
             // echo $paciente;
-            $paciente = Paciente::getUnPaciente($_POST['id']);
-            if($paciente == false){
-                echo json_encode(array(
-                    "estado" => false
-                ));
-            }else{
-                echo json_encode(array(
-                    "estado" => true
-                ));
-               setcookie("id", $paciente['identidad'], time()+(60*60*24*31), "/");
+            if(isset($_GET['id'])){
+                $paciente = Paciente::getUnPaciente($_GET['id']);
+                if($paciente == false){
+                    echo json_encode(array(
+                        "estado" => false
+                    ));
+                }else{
+                    echo json_encode(array(
+                        "estado" => true,
+                        "paciente" => $paciente
+                    ));
+                    setcookie("id", $paciente['identidad'], time()+(60*60*24*31), "/");
+                }
             }
         break;
         case 'PUT':
