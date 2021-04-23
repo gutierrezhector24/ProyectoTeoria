af78a759b0d53ac10519f05c33980d51f696945b
Ángel <anrealba9@gmail.com>
2021-04-22 22:12:52 -0600

    History algo hecha, solo falta graficar

---
+++ proyectoCovid/JS/history.js
@@ -0,0 +1,10 @@
+function leerCookie(identificador) {
+    let name = identificador + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return unescape(c.substring(name.length, c.length));
+    }
+    return "";
+}
